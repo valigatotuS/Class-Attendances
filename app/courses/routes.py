b6fafc35fabf93540d8f5b1f208d5baed000bdeb
valigatotuS <valentin.quevy@gmail.com>
2022-02-28 23:09:49 +0100

    update courses-page(py&html&css) and add sql-querie

--- app/courses/routes.py
+++ app/courses/routes.py
@@ -2,18 +2,17 @@ from flask import Blueprint, render_template, request, redirect, current_app
 from app.courses import courses_bp
 from flask_login import current_user, login_user, logout_user, login_required
 from app.database.models import User, Class, UCourse, Attendance, Course
-
-
+from app.database import queries
 
 @courses_bp.route('/courses', methods=['GET','POST'])
 @login_required
 def courses():
-    id = current_user.get_id()
-    ucs = UCourse.query.filter_by(user_id=id).all()
-    courses_id = [uc.course_id for uc in ucs]
-    courses = Course.query.filter(Course.id.in_(courses_id)).all()
-    names = [course.name for course in courses]
-    output = names
+    courses = queries.get_user_courses()
+    tt = queries.get_user_courses_v2()
+    return render_template("courses/courses.html.jinja", courses=courses)
 
-    return render_template("courses/courses.html.jinja", courses=courses) #str(names)
-    
+@courses_bp.route('/coursesQ', methods=['GET','POST'])
+def coursesq():
+    courses = queries.get_user_courses_v2().all()
+    out = [[course.name, course.semester, course.role] for course in courses]
+    return render_template("courses/courses.html.jinja", courses=courses)
--- app/database/queries.py
+++ app/database/queries.py
@@ -1,5 +1,8 @@
 from app.database.models import User
 from sqlalchemy import insert
+from flask_login import current_user
+from app.database.models import User, Class, UCourse, Attendance, Course
+from app import db2
 
 def post_user(db_, values:list): 
     id_ = db_.Execute("SELECT COUNT(*) FROM User") 
@@ -15,6 +18,23 @@ def get_user_info_bymail(db_, email:str):
         WHERE u.email='{email}';""", as_json=True)
     if len(res)<1: res = [None] 
     return res[0]
+
+def get_user_courses():
+    id = current_user.get_id()
+    ucs = UCourse.query.filter_by(user_id=id).all()
+    courses_id = [uc.course_id for uc in ucs]
+    courses = Course.query.filter(Course.id.in_(courses_id)).all()
+    return courses
+
+def get_user_courses_v2():
+    querie = f"""
+        SELECT c.name, c.semester, uc.role, c.id
+        FROM User u
+        INNER JOIN UCourse uc ON u.id=uc.user_id
+        INNER JOIN Course c ON uc.course_id=c.id
+        WHERE u.id={current_user.get_id()};"""
+    courses = db2.session.execute(querie)
+    return courses
 
 def add_user(fname,lname,email,password_hash,db):
     u = User(
